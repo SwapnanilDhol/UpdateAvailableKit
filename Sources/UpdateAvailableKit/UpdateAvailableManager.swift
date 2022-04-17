e46ffc107b280ef2496f2f0a5b6fb8764150751a
Rudrank Riyam <rudrankriyam@gmail.com>
2022-04-17 21:53:04 +0530

    Add async method

--- Sources/UpdateAvailableKit/UpdateAvailableManager.swift
+++ Sources/UpdateAvailableKit/UpdateAvailableManager.swift
@@ -11,6 +11,20 @@
 
 import Foundation
 
+enum UpdateAvailableKitError: Error {
+    case appStoreVersionMissing(for: ITunesLookupResult?)
+}
+
+extension UpdateAvailableKitError: CustomStringConvertible {
+    var description: String {
+        switch self {
+#warning("Add relevant error description")
+            case .appStoreVersionMissing(let result):
+                return "App Store version missing for \(String(describing: result))"
+        }
+    }
+}
+
 public final class UpdateAvailableManager {
 
     public static let shared = UpdateAvailableManager()
@@ -18,6 +32,58 @@ public final class UpdateAvailableManager {
     private init() { }
 
     // MARK: - Version Update
+    @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
+    public func checkForVersionUpdate(
+        with bundleID: String = Bundle.main.bundleIdentifier,
+        currentVersion: String = Bundle.main.releaseVersionNumber,
+        useCache: Bool = true) async throws -> UpdateAvailableResult {
+            guard let url: URL = .createITunesLookupURL(with: bundleID) else {
+                throw URLError(.badURL)
+            }
+
+            if useCache {
+                if let cachedData = UserDefaults.standard.data(forKey: cacheKey),
+                   let response = try? JSONDecoder().decode(LookupCachableResponse.self, from: cachedData),
+                   let currentAppStoreVersion = response.response.results?.first?.version,
+                   Date() < response.expiryDate {
+                    if self.isAppStoreVersionGreaterThanCurrentVersion(
+                        appStoreVersion: currentAppStoreVersion,
+                        currentVersion: currentVersion
+                    ) {
+                        return .updateAvailable(newVersion: currentAppStoreVersion)
+                    } else {
+                        return .noUpdatesAvailable
+                    }
+                } else {
+#warning("Throw relevant error")
+                    throw URLError(.badServerResponse)
+                }
+            } else {
+                let (data, _) = try await URLSession.shared.data(from: url)
+                let response = try JSONDecoder().decode(ITunesLookupResponse.self, from: data)
+
+                guard let currentAppStoreVersion = response.results?.first?.version else {
+                    throw UpdateAvailableKitError.appStoreVersionMissing(for: response.results?.first)
+                }
+
+                let cachableReponse: LookupCachableResponse = .init(
+                    expiryDate: Date().addingTimeInterval(3600),
+                    response: response
+                )
+
+                let cachedResponseData = try JSONEncoder().encode(cachableReponse)
+                UserDefaults.standard.set(cachedResponseData, forKey: self.cacheKey)
+
+                if isAppStoreVersionGreaterThanCurrentVersion(
+                    appStoreVersion: currentAppStoreVersion,
+                    currentVersion: currentVersion
+                ) {
+                    return .updateAvailable(newVersion: currentAppStoreVersion)
+                } else {
+                    return .noUpdatesAvailable
+                }
+            }
+        }
 
     public func checkForVersionUpdate(
         with bundleID: String = Bundle.main.bundleIdentifier,
